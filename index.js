5e625d805db5170d7aaf6902c505750105d0af74
William Buggy <somecolormage@live.com.au>
2021-09-29 15:06:25 +1000

    make sure node and heroku are working

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 80; //instructions gives no specific port so assuming standard HTTP port until proven otherwise
+
+//the following is mostly test code to make sure I've got node and heroku functional, expect most of this to go away
+const server = http.createServer();
+server.on('request', (request, response) => {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/plain')
+    response.end('Hello World\n')
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
