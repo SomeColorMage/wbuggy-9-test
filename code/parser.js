8f0ea95c0da2f964290632753696b00540d861e9
William Buggy <somecolormage@live.com.au>
2021-10-02 14:56:45 +1000

    basic outline of order of events in JSON parser

---
+++ code/parser.js
@@ -0,0 +1,29 @@
+//there's two elements to this... the return must indicate if the parse passed or failed, and it must contain the new JSON string to send back
+class returnObj {
+    constructor() {
+        this.status = 200;
+        this.returnJSON = undefined;
+    }
+}
+
+//input should be the string from the POST request
+function parseJson(input) {
+    let result = new returnObj();
+
+    try {
+        let parsedJSON = JSON.parse(input);
+
+        //based on the example JSON, we're looking for a key called 'payload'
+        //if we can't find that, not sure, it's technically not invalid JSON so an empty response might be fine
+        //from there, check each entry for entries with drm=true and episodeCount>0
+        //for ones where both of those exist, copy down image, slug and title (as a string to prevent XSS issues, just in case)
+        //then form the response JSON
+    } catch (error) {
+        //process this then return
+        result.status = 400;
+
+        //need a proper response JSON here in format { "error": "Could not decode request: <error here>" }
+    }
+
+    return result;
+}
