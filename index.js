15e1e7133a17db0f228a12a5521963f81404df1c
William Buggy <somecolormage@live.com.au>
2021-10-04 08:28:42 +1100

    server setup

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const http = require('http');
+const parseJson = require('./code/parser').parseJson;
 
-const hostname = '127.0.0.1';
-const port = 80; //instructions gives no specific port so assuming standard HTTP port until proven otherwise
+http.createServer((request, response) => {
+	if(request.method === 'POST') {
+		let body = [];
+		request.on('error', (err) => {
+			//something's gone wrong, so let's try to send back an error in the desired error format
+			let errorObj = new Object();
+			errorObj.error = "Could not decode request: " + err.message;
 
-//the following is mostly test code to make sure I've got node and heroku functional, expect most of this to go away
-const server = http.createServer();
-server.on('request', (request, response) => {
-	response.statusCode = 200
-	response.setHeader('Content-Type', 'text/plain')
-	response.end('Hello World\n')
-});
+			response.statusCode = 400;
+			response.setHeader('Content-Type', 'application/json');
+			response.write(JSON.stringify(errorObj));
+			response.end();
+		}).on('data', (chunk) => {
+			body.push(chunk);
+		}).on('end', () => {
+			//turn the buffer into a string
+			body = Buffer.concat(body).toString();
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`)
-});
+			//now read the string...
+			let responseData = parseJson(body);
+
+			//...and transmit the result back
+			response.statusCode = responseData.status;
+			response.setHeader('Content-Type', 'application/json');
+			response.write(responseData.returnJSON);
+			response.end();
+		});
+	} else {
+		//POST requests only, ignore everything else
+		let errorObj = new Object();
+		errorObj.error = "Could not decode request: POST requests only";
+
+		response.statusCode = 400;
+		response.setHeader('Content-Type', 'application/json');
+		response.write(JSON.stringify(errorObj));
+		response.end();
+	}
+}).listen(80);
